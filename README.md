e2e8e95d8c039cbabc8059c480b78ccfb952ea0f
Ben Lyaunzon <ben@skycatch.com>
2014-03-26 17:48:42 -0700

    Improve Readme, add more functionality, and improve tests

--- README.md
+++ README.md
@@ -1,24 +1,36 @@
 # Box API Node.JS Client
 
+## Install
+
+    npm install nodejs-box
+
 ## Setup
 
 ```javascript
-
-var Box = require('node-box');
+var Box = require('nodejs-box');
 
 var box = new Box();
-
 ```
 
 ## Folders
 
-### box.folders.info(folder_id)
+### box.folders.info(folder_id, callback)
 
 Returns info given a folder ID
 
-### box.folders.root
+### box.folders.root(callback)
 
 Returns info for the root folder (ID '0')
+
+### box.folders.create(folder_name, parent_folder_id, callback)
+
+Creates a new folder, given a name and the ID of the parent folder
+
+### box.folders.delete(folder_id, recursive*, callback)
+
+Deletes a folder, given a folder ID.
+Optionally takes a second argument, 'recursive', that holds a boolean value, which,
+if true, will delete the folder even if it is not empty.
 
 ## Files
 
@@ -29,3 +41,9 @@ Uploads a file given a path and a folder ID
 ### box.files.createMetadata(metadata, file_id)
 
 Create metadata for a given file (ID)
+
+Note: Your app/account has to be authorized for the Metadata API beta to use this.
+
+## Test
+
+Run `export access_token='YOUR_TOKEN_GOES_HERE' && npm test`
--- test/index.js
+++ test/index.js
@@ -1,27 +1,106 @@
-var Box = require('../lib/');
+var Box = require('../lib/'),
+    test = require('prova');
 
-var box = new Box();
+var box = new Box({
+  access_token: process.env.access_token
+});
 
-box.files.upload('img/cat.jpg','1776997242', function(err, res){
-  if(err){
-    console.log(err);
-  }else{
-    var file_id = res.entries[0].id;
 
-    var metadata = {
-      'breed' : 'burmese',
-      'country' : 'Burma',
-      'origin': 'natural',
-      'coat': 'short',
-      'pattern': 'solid'
-    };
+test('Module', function(assert){
+  assert.ok(box.options);
+  assert.ok(box.options.access_token);
+  assert.ok(box.files);
+  assert.ok(box.folders);
+  assert.end();
+});
 
-    box.files.createMetadata(file_id, metadata, function(err, res){
-      if(err)
-        console.log(err);
-      else
-        console.log(res);
+test('Resource: Folders', function(t){
+
+  t.test('Method: root',function(assert){
+    box.folders.root(function(err, res){
+      assert.notOk(err);
+      assert.ok(res);
+      assert.end();
+    });
+  });
+
+  var testFolderID = null,
+      testFolderName = 'Test Folder';
+
+  t.test('Method: create', function(assert){
+    box.folders.create(testFolderName, '0', function(err, res){
+      assert.notOk(err);
+      assert.ok(res);
+
+      testFolderID = res.id;
+
+      assert.end();
+    });
+  });
+
+  t.test('Method: info', function(assert){
+    box.folders.info(testFolderID, function(err, res){
+
+      assert.notOk(err);
+      assert.ok(res);
+
+      assert.equal(res.id, testFolderID);
+      assert.equal(res.name, testFolderName);
+
+      assert.end();
+    });
+  });
+
+  t.test('Method: delete', function(assert){
+    box.folders.delete(testFolderID, function(err, res){
+      assert.notOk(err);
+      assert.ok(res);
+
+      assert.deepEqual(res, {});
+
+      assert.end();
+    });
+  });
+
+
+});
+
+test('Resource: Files', function(t){
+  function before(callback){
+    box.folders.create('Test Folder 2', '0', function(err, res){
+      if(err) return callback(err);
+
+      callback(null, res.id);
     });
   }
 
+  function after(id, callback){
+    box.folders.delete(id, function(err, res){
+      if(err) return callback(err);
+
+      callback(null, res);
+    });
+  }
+
+  before(function(err, folderID){
+    t.notOk(err);
+
+    t.test('Method: upload', function(assert){
+      box.files.upload('./test/img/cat.jpg', folderID, function(err, res){
+        assert.notOk(err);
+        assert.ok(res);
+        assert.end();
+      });
+    });
+
+    after(folderID, function(err, res){
+      t.notOk(err);
+      t.ok(res);
+
+      t.deepEqual(res, {});
+
+
+      t.end();
+    });
+  });
 });
